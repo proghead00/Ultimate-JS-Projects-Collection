415550d7cf8c79957c5b3b026501e34da6308bbc
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2020-10-15 02:38:04 +0530

    Initial Commit

---
+++ Modal-Menu/script.js
@@ -0,0 +1,33 @@
+const toggle = document.getElementById("toggle")
+
+const close = document.getElementById("close")
+
+
+const open = document.getElementById("open")
+
+
+const modal = document.getElementById("modal")
+
+
+//toggle
+
+toggle.addEventListener("click", ()=>
+document.body.classList.toggle('show-nav'))
+
+
+
+// show modal
+
+open.addEventListener("click", () => modal.classList.add('show-modal'))
+
+
+// hide modal
+
+close.addEventListener("click", ()=> modal.classList.remove('show-modal'))
+
+
+// hide modal on outside click
+
+window.addEventListener("click", e=> e.target == modal ? 
+modal.classList.remove('show-modal') :false)
+
